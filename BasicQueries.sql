102354a718fdd2d50431f3db972ce6e684a50f14
Alison Reilly <reillyamr@gmail.com>
2021-04-15 13:33:25 -0400

    add more examples to basic

--- BasicQueries.sql
+++ BasicQueries.sql
@@ -15,4 +15,21 @@ UPDATE contacts SET last_name = 'Reilly' WHERE id=1;
 -- DELETE - remove one or more rows
 DELETE FROM contacts WHERE id=2;
 
+-- a most basic query can even be from string no db
+SELECT 'Hello', 'World'
+-- retrieves a table structure of |Hello|World|
 
+-- overall abstracted use
+SELECT <column_name>, <column_name> FROM <table_name>;
+
+-- wildcard usage
+SELECT * FROM <table_name>;
+
+-- because from a FROM clause you could refer to multiple tables - good practice to 'qualify' the column with the table name before
+SELECT person.first_name, person.last_name FROM person;
+-- but as a short cut can also alias that table name to make it shorter:
+SELECT p.first_name, p.last_name FROM person p;
+
+
+-- this is the marker of a comment
+/* but this can also be used as well */
